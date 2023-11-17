d15077ba7f360bfc1759f0a02766286c9e480fe6
AndreDulla <77928062+AndreDulla@users.noreply.github.com>
2023-11-17 17:56:28 +0100

    Now the same card cannot be extracted twice.

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,11 @@
 public class Deck {
     private int cardValue;
     private int casualNumber;
+    private boolean[] extractedCards;
+
+    public Deck(){
+        for(int i = 0; i < 104; i++) extractedCards[i] = false;
+    }
 
 
     /**
@@ -8,7 +13,11 @@ public class Deck {
      * @return value of the extracted card
      */
     public int getNewCardValue(){
-        casualNumber = getRandomNumber(1, 104);
+        do {
+            casualNumber = getRandomNumber(1, 104);     // searching a casual card not already extracted
+        }while(!extractedCards[casualNumber]);
+
+        extractedCards[casualNumber] = true;    // remembering that this card has already been extracted
         cardValue = (casualNumber % 13 + 1);  // finding the value of the card (there are 13 card for each seed)
         if(cardValue > 10) cardValue = 10;  // every figure value is 10
         return cardValue;
