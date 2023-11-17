33e1f5033d9e72dd3bc68174e11ea403c5c55fa4
AndreDulla <77928062+AndreDulla@users.noreply.github.com>
2023-11-17 17:41:55 +0100

    Translations

--- src/Deck.java
+++ src/Deck.java
@@ -1,14 +1,15 @@
-public class Mazzo {
+public class Deck {
     private int cardValue;
-    private int numCasuale;
+    private int casualNumber;
+
 
     /**
      * Extracts a card from the deck
      * @return value of the extracted card
      */
     public int getNewCardValue(){
-        numCasuale = getRandomNumber(1, 104);
-        cardValue = (numCasuale % 13 + 1);  // finding the value of the card (there are 13 card for each seed)
+        casualNumber = getRandomNumber(1, 104);
+        cardValue = (casualNumber % 13 + 1);  // finding the value of the card (there are 13 card for each seed)
         if(cardValue > 10) cardValue = 10;  // every figure value is 10
         return cardValue;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome in blackjack");
+
+        Deck deck = new Deck();
+        System.out.println(deck.getNewCardValue());
     }
 }
