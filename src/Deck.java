214c7046701a2bde6f7816503ba98ff5046dad5e
AndreDulla <77928062+AndreDulla@users.noreply.github.com>
2023-11-17 20:48:37 +0100

    add: When all the cards have been drawn, it fills the deck array with false

--- src/Deck.java
+++ src/Deck.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Deck {
     private int cardValue;
     private int casualNumber;
-    private boolean[] extractedCards;
+    private boolean[] extractedCards = new boolean[104];
+    private int manyExtractedCards = 0;
 
     public Deck(){
         for(int i = 0; i < 104; i++) extractedCards[i] = false;
@@ -13,14 +16,22 @@ public class Deck {
      * @return value of the extracted card
      */
     public int getNewCardValue(){
+        if(manyExtractedCards == 104) Arrays.fill(extractedCards, false);
+
         do {
             casualNumber = getRandomNumber(1, 104);     // searching a casual card not already extracted
-        }while(!extractedCards[casualNumber]);
+        }while(extractedCards[casualNumber]);
 
+        manyExtractedCards++;   // adding one to the counter of the cards being extracted
         extractedCards[casualNumber] = true;    // remembering that this card has already been extracted
+
         cardValue = (casualNumber % 13 + 1);  // finding the value of the card (there are 13 card for each seed)
         if(cardValue > 10) cardValue = 10;  // every figure value is 10
         return cardValue;
+    }
+
+    private void everythingFalse(boolean[] array){
+
     }
 
     /**
@@ -32,5 +43,4 @@ public class Deck {
     private int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
-
 }
